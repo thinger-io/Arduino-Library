111ed1f475b3cc2eac9908e844c6abf4da22b402
alvarolb <alvarolb@gmail.com>
2018-03-12 19:26:18 +0100

    added default example for NodeMCU

---
+++ examples/ESP8266/NodeMCU/NodeMCU.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+#include <ThingerESP8266.h>
+
+#define USERNAME "your_user_name"
+#define DEVICE_ID "your_device_id"
+#define DEVICE_CREDENTIAL "your_device_credential"
+
+#define SSID "your_wifi_ssid"
+#define SSID_PASSWORD "your_wifi_ssid_password"
+
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  thing.add_wifi(SSID, SSID_PASSWORD);
+
+  // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
+  thing["led"] << digitalPin(LED_BUILTIN);
+
+  // resource output example (i.e. reading a sensor value)
+  thing["millis"] >> outputValue(millis());
+
+  // more details at http://docs.thinger.io/arduino/
+}
+
+void loop() {
+  thing.handle();
+}
