d518c8f321f0fae6109e6bcb010a2931663f9ba9
alvarolb <alvarolb@gmail.com>
2018-03-13 09:10:07 +0100

    updated arduino yun example with LED_BUILTIN

--- examples/Arduino/ArduinoYun/ArduinoYun.ino
+++ examples/Arduino/ArduinoYun/ArduinoYun.ino
@@ -6,19 +6,17 @@
 #define DEVICE_CREDENTIAL "your_device_credential"
 
 ThingerYun thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
-const int ledPin =  13;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // initialize bridge
   Bridge.begin();
 
   // pin control example (i.e. turning on/off a light, a relay, etc)
-  thing["led"] << digitalPin(ledPin);
+  thing["led"] << digitalPin(LED_BUILTIN);
 
   // resource output example (i.e. reading a sensor value, a variable, etc)
-  thing["millis"] >> outputValue(millis());
 
   // more details at http://docs.thinger.io/arduino/
 }
