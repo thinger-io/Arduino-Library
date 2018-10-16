340c3725f5996bba65b30656f72b63755e37f7f4
alvarolb <alvarolb@gmail.com>
2018-10-16 02:24:25 +0200

    Added example for Arduino Wifi MKR 1010

---
+++ examples/Arduino/ArduinoMKR1010/ArduinoMKR1010.ino
@@ -0,0 +1,30 @@
+#include <WiFiNINA.h>
+#include <ThingerWifi101.h>
+
+#define USERNAME "your_user_name"
+#define DEVICE_ID "your_device_id"
+#define DEVICE_CREDENTIAL "your_device_credential"
+
+#define SSID "your_wifi_ssid"
+#define SSID_PASSWORD "your_wifi_ssid_password"
+
+ThingerWifi101 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+void setup() {
+  // configure wifi network
+  thing.add_wifi(SSID, SSID_PASSWORD);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // pin control example (i.e. turning on/off a light, a relay, etc)
+  thing["led"] << digitalPin(LED_BUILTIN);
+
+  // resource output example (i.e. reading a sensor value, a variable, etc)
+  thing["millis"] >> outputValue(millis());
+
+  // more details at http://docs.thinger.io/arduino/
+}
+
+void loop() {
+  thing.handle();
+}
